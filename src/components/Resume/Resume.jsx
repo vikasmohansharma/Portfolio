043a2a3bb7258ebfa810c4d21a30d7f8bce1d5fa
Your Name <you@example.com>
2024-05-14 21:37:23 +0530

    Deployement step 5

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import DownloadButton from "./DownloadResume/Download";
 import "./Resume.scss";
 
-const Resume = () => {
+/* const Resume = () => {
   return (
     <div className="resume">
       <br />
@@ -25,24 +25,64 @@ const Resume = () => {
       <br />
       <br />
       <br />
-      <div></div>
-      
+      <div>
+      <embed src="resume.pdf" type="application/pdf" width="100%" height="600px" alt="Loading RESUME.."/>
+     
+    </div>    
+    <div>
+      <object data="resume.pdf" type="application/pdf" width="100%" height="600px">
+        Your browser doesn't support embedded PDFs. <a href="/path/to/your/pdf/file.pdf">Click here to download the PDF.</a>
+      </object>
+    </div>  
       <DownloadButton
-        file="downloadimage.jpg"
+        file="resume.pdf"
         filename="desiredFilename.jpg"
       />
       <button
         onClick={(e) => {
           window.open(
-            "downloadimage.jpg",
+            "resume.pdf",
             "_blank"
           );
         }}
       >
-        DOWNLOAD TWO
+        View In Separate Window
       </button>
     </div>
   );
-};
+}; */
 
+function Resume() {
+
+  const buttonStyle = {
+    padding: '10px 20px',
+    fontSize: '16px',
+    borderRadius: '5px',
+    backgroundColor: '#007bff',
+    color: 'white',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s',
+    marginRight: '10px', // Add margin to create space between buttons
+  };
+
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <DownloadButton /><br />
+        <button style={{ ...buttonStyle, backgroundColor: '#28a745' }}
+        onClick={(e) => {
+          window.open(
+            "resume.pdf",
+            "_blank"
+          );
+        }}
+      >
+        View Resume
+      </button>
+      </div>
+    </div>
+  );
+
+}
 export default Resume;
